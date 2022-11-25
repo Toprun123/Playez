90967aa7f302995c91d4f346ef657025c8cf2da2
Toprun123 <syeddaanish2000@gmail.com>
2022-11-25 21:20:23 +0300

    Initialise Database

--- cs50w_fp/settings.py
+++ cs50w_fp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playez'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cs50w_fp.wsgi.application'
 
+AUTH_USER_MODEL = "playez.User"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
