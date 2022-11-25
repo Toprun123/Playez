289b03f7ef9a44421fd5c0b3d88b77c979c9d5ce
Toprun123 <syeddaanish2000@gmail.com>
2022-11-25 21:21:10 +0300

    Basic changes

--- playez/views.py
+++ playez/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from .models import *
+
+import json
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('hello')
