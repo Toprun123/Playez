e83dd1e691d0c8cbfc60f0ac472b1da22e1b0161
Toprun123 <syeddaanish2000@gmail.com>
2022-11-27 22:51:05 +0300

    Set debug to false and serve static file seperately

--- cs50w_fp/settings.py
+++ cs50w_fp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$clll41qz2jevalf!(f+%pbr-sj!-%89e@(a6&z^^u$i$(l!14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'playez/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
